390dfe873801773842c271ac3bfd9709ceac78b6
sougasdanilo@gmail.com <sougasdanilo>
2026-03-24 10:14:40 -0300

    aff

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: './',
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Pedidos App',
-        short_name: 'Pedidos',
-        theme_color: '#6200ff',
-        background_color: '#000000',
-        display: 'standalone',
-        icons: [
-          {
-            src: './icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    react()
   ]
 })
