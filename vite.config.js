f4f9a5fa52a9b0bbb607728a55dc718261e73ae8
sougasdanilo@gmail.com <sougasdanilo>
2026-03-24 10:06:20 -0300

    up

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     VitePWA({
@@ -15,7 +16,7 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/icon-192.png',
+            src: './icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           }
